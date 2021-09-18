faacefe62085b59d7638ae455ac5af02912fc226
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-09-18 11:19:22 -0400

    basic page, styles and js started

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var startButtonEl = document.querySelector("#start-btn");
+
+var startQuiz = function() {
+    console.log("quiz started")
+};
+
+
+startButtonEl.addEventListener("click", startQuiz)
