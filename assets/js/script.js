1d4e28b018d4783a90fda880b94c0fc3c5d5b537
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-09-18 22:49:27 -0400

    added starter js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,61 @@
+var quizQuestions = [
+    {
+        name: 1,
+        question: "True or false: HTML is an acronym for 'HyperText Markdown Langauge'?",
+        answers: [
+            "true",
+            "false"
+        ],
+        correctAns: "false"
+    },
+    {
+        name: 2,
+        question: "In an HTML document, which of the following should be located in the document 'head'?",
+        answers: [
+            "Any links to Stylesheets",
+            "The header element",
+            "Text for the footer element",
+            "Granny Smith apples"
+        ],
+        correctAns: "Any links to Stylesheets"
+    },
+    {
+        name: 3,
+        question: "this is question 3?",
+        answers: [
+            "yes",
+            "no",
+            "maybe"
+        ],
+        correctAns: "yes"
+    },
+    {
+        name: 4,
+        question: "This is question 4?",
+        answers: [
+            "yes",
+            "no",
+            "maybe"
+        ],
+        correctAns: "yes"
+    },
+    {
+        name: 5,
+        question: "This is question 5?",
+        answers: [
+            "yes",
+            "no",
+            "maybe"
+        ],
+        correctAns: "yes"
+    }
+];
+
 var startButtonEl = document.querySelector("#start-btn");
+
+
+
+
 
 var startQuiz = function() {
     console.log("quiz started")
